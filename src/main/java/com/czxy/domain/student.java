71b9d2cd20d96833b85b8d0fecc479c94f0781ac
hj <hj_12_28@163.com>
2020-08-13 09:09:53 +0800

    增加student

---
+++ src/main/java/com/czxy/domain/student.java
@@ -0,0 +1,13 @@
+package com.czxy.domain;
+
+import lombok.Data;
+
+/**
+ * @Classname hejun
+ * @Date 2020/8/13 9:04
+ */
+@Data
+public class student {
+    private  String name;
+    private  String age;
+}
