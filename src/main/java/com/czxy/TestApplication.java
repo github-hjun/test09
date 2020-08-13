f3ae523f3dcc840b30d6c24134e04d1860c06d51
hj <hj_12_28@163.com>
2020-08-13 09:03:23 +0800

    Initial commit

---
+++ src/main/java/com/czxy/TestApplication.java
@@ -0,0 +1,15 @@
+package com.czxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Classname hejun
+ * @Date 2020/8/13 8:57
+ */
+@SpringBootApplication
+public class TestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class,args);
+    }
+}
